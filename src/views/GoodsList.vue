627ca539dc92d1500e484b8db3b7b771fdb670f4
YunXinZhiCheng <867395636@qq.com>
2021-09-04 19:08:27 +0800

    Composition API 基本使用

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -1,19 +1,12 @@
-
 <template>
-<el-table
-    :data="tableData"
-    style="width: 100%">
-    <el-table-column
-      label="日期"
-      width="180">
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column label="日期" width="180">
       <template #default="scope">
         <i class="el-icon-time"></i>
         <span style="margin-left: 10px">{{ scope.row.date }}</span>
       </template>
     </el-table-column>
-    <el-table-column
-      label="姓名"
-      width="180">
+    <el-table-column label="姓名" width="180">
       <template #default="scope">
         <el-popover effect="light" trigger="hover" placement="top">
           <template #default>
@@ -30,52 +23,60 @@
     </el-table-column>
     <el-table-column label="操作">
       <template #default="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+        <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">
+          编辑
+        </el-button>
         <el-button
           size="mini"
           type="danger"
-          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          @click="handleDelete(scope.$index, scope.row)"
+        >
+          删除
+        </el-button>
       </template>
     </el-table-column>
   </el-table>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }]
-      }
+import { reactive, onMounted } from 'vue'
+// 封装函数：用于存放数据
+function loadData(data) {
+  // 往数组添加数据
+  data.push(
+    {
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄',
     },
-    methods: {
-      handleEdit(index, row) {
-        console.log(index, row);
-      },
-      handleDelete(index, row) {
-        console.log(index, row);
-      }
+    {
+      date: '2016-05-04',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1517 弄',
+    },
+    {
+      date: '2016-05-01',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1519 弄',
     }
-  }
+  )
+  // 返回数据
+  return data
+}
+export default {
+  setup() {
+    // 表格数据
+    const tableData = reactive([])
+    // 钩子函数
+    onMounted(() => {
+      // 调用函数：加载数据
+      loadData(tableData)
+    })
+    return {
+      tableData,
+    }
+  },
+}
 </script>
 
-<style>
-
-</style>
+<style></style>
